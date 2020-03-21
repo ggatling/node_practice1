56d91b1788f9e59316d8008bef7240d4254d0058
Gerda Gatling <gerdagatling@Gerdas-MacBook-Pro.local>
2020-03-21 12:12:41 -0400

    created index.js and brought in json object

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+
+import logo from './logo.svg';
+import './App.css';
+import React, {Component} from 'react';
+
+
+class App extends Component {
+  state = { users: []}
+  componentDidMount(){
+    fetch('/users')
+      .then (res=> res.json())
+      .then (users => this.setState({ users }));
+  };
+  render(){
+    return (
+      <div className="App">
+        <h1>Users</h1>
+        <ul>
+          {this.state.users.map(user =>
+            <li key={user.id}>{user.username}</li>
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
